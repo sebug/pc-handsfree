90abaeef4496a7e9aa434075bb0d82bc61c1b4ee
sebug <sebastian.gfeller@gmail.com>
2020-04-24 11:19:41 +0200

    Defining grammar outside

--- PCHandsfree/Pages/Index.cshtml.cs
+++ PCHandsfree/Pages/Index.cshtml.cs
@@ -9,6 +9,11 @@ namespace PCHandsfree.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Grammar { get; set; } = "#JSGF V1.0; grammar radios; 1 | 2 | 3 | 4 | 5 | " +
+    " 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | " +
+    " 15 | 16 | 17 | 18 | 19 | 20 | 21 | " +
+    " 22 | 23 | 24 ;";
+
         public void OnGet()
         {
         }
--- PCHandsfree/wwwroot/site.js
+++ PCHandsfree/wwwroot/site.js
@@ -1,12 +1,8 @@
-﻿const grammar = '#JSGF V1.0; grammar radios; one | two | three | four | five | ' +
-    ' six | seven | eight | nine | ten | eleven | twelve | thirteen | fourteen | ' +
-    ' fifteen | sixteen | seventeen | eighteen | nineteen | twenty | twenty-one | ' +
-    ' twenty-two | twenty-three | twenty-four ;';
-const recognition = new webkitSpeechRecognition();
+﻿const recognition = new webkitSpeechRecognition();
 const speechRecognitionList = new webkitSpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.lang = 'fr-CH';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
